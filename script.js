cb55ea8bd4f3790ceb5d07cafb96b0d4add5d6a2
Alexey <a.kashutin.dev@gmail.com>
2021-01-08 21:04:58 +0600

    add light-theme and dark-theme

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-// let inputs = primary.getElementsByTagName('img');
-
-// function premiumOnclick() {
-//     if (premiumImg.contextmenu) {
-//         sniperImg.innerHTML = false;
-//     }
+// function showSpecial() {
+//     var element = document.getElementsByClassName('P');
+//     element.style.visibility = 'hidden';
 // }
-// premiumOnclick()
+
+//=======================================================================
+
+//  document.getElementsByClassName("premium").onclick = function() {
+//      document.getElementById("P").style.display = "none";
+//  }
+
+//=======================================================================
+
+//  let element = document.getElementById('P');
+
+//  function hidePremium() {
+//      element.style.display = "none";
+//  }
+
+//=======================================================================
+
+  // let element = document.querySelector('.premium');
+
+  // element.onclick = function() {
+  //     console.log("1");
+  // }
+
+let page = document.querySelector('.page');
+let themeButton = document.querySelector('.theme-button');
+
+themeButton.onclick = function() {
+  page.classList.toggle('light-theme');
+  page.classList.toggle('dark-theme');
+};
