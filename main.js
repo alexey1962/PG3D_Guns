776e890e5c0db5cb360b373e1d116eae42cb6345
Alexey <a.kashutin.dev@gmail.com>
2021-02-02 20:01:07 +0600

    add bottom links

---
+++ main.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+    var bottom = new Vue({
+        el: '#bottom',
+        data: {
+            info: "Связи: ",
+            link: "http://pixelgun.wikia.com/wiki/Pixel_Gun_Wiki",
+            play: "https://play.google.com/store/apps/details?id=com.pixel.gun3d&hl=ru&gl=US"
+        }
+    });
+}
